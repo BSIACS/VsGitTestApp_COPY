f19f4f95c183ad2d7c052d40c905b9c498075e30
Serg <serg@example.com>
2020-11-29 22:13:05 +0300

    Добавьте файлы проекта.

---
+++ VsGitTestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VsGitTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
